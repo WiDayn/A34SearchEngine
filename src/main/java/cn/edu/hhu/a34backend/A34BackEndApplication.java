5da8b7b313011c7208e44f18cd489706606f93e2
WiDayn <widayn@prmlk.com>
2022-12-24 17:03:27 +0800

    Initial commit

---
+++ src/main/java/cn/edu/hhu/a34backend/A34BackEndApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hhu.a34backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A34BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A34BackEndApplication.class, args);
+    }
+
+}
